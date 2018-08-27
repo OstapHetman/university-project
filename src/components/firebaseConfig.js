72ebd8ab050db518d519475060d6c51ab85f921d
OstapHetman <prickly4528@gmail.com>
2018-08-27 10:43:21 +0300

    init firestore

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDDN4Wt9S8aB2AFUhDRl8066vydGa57Zzw",
+    authDomain: "nulp-ee07f.firebaseapp.com",
+    databaseURL: "https://nulp-ee07f.firebaseio.com",
+    projectId: "nulp-ee07f",
+    storageBucket: "nulp-ee07f.appspot.com",
+    messagingSenderId: "671480169055"
+}
